ab403bff5b71f70dd2058c775ba6efa60d7515d2
minyez <minyez@iphy.ac.cn>
2022-11-04 12:13:52 +0800

    data dispatch from first process

--- main.cpp
+++ main.cpp
@@ -22,19 +22,18 @@ int main (int argc, char *argv[])
     const int IRSRC = 0, ICSRC = 0;
 
     // define the matrices, with no data, i.e. no memory allocated
-    matrix<double> mat1, mat2;
+    matrix<double> mat1(1, 1, MAJOR::COL), mat2(1, 1, MAJOR::COL), prod(1, 1, MAJOR::COL);
     // auto hemat = random_he(5, complex<double>{0, 0}, complex<double>{1, 0});
     // auto hemat_inv = inverse(hemat);
     // printf("hemat*hemat_inv\n%s", str(hemat*hemat_inv).c_str());
 
-
-    mat1.resize(m, k);
-    mat2.resize(k, n);
-    mat1.random(0, 2);
-    mat2.random(-1, 1);
-
     if (blacs_h.myid == 0)
     {
+        mat1.resize(m, k);
+        mat2.resize(k, n);
+        prod.resize(m, n);
+        mat1.random(0, 2);
+        mat2.random(-1, 1);
         printf("Full mat1\n%s", str(mat1).c_str());
         // printf("mathe\n%s", str(hemat).c_str());
         printf("Full mat2\n%s", str(mat2).c_str());
@@ -43,36 +42,27 @@ int main (int argc, char *argv[])
     }
     blacs_h.barrier();
 
-    // array descriptor of own process
+    // array descriptor for storage of distributed data
     ArrayDesc mat1_desc(blacs_h), mat2_desc(blacs_h), prod_desc(blacs_h);
+    // array descriptor. Full block descriptor to store global data only in the first process
+    ArrayDesc mat1_block_desc(blacs_h), mat2_block_desc(blacs_h), prod_block_desc(blacs_h);
     mat1_desc.init(m, k, mb, kb, IRSRC, ICSRC);
+    mat1_block_desc.init(m, k, m, k, IRSRC, ICSRC);
     mat2_desc.init(k, n, kb, nb, IRSRC, ICSRC);
+    mat2_block_desc.init(k, n, k, n, IRSRC, ICSRC);
     prod_desc.init(m, n, mb, nb, IRSRC, ICSRC);
-    // matrix<double> mat1_local = init_local_mat<decltype(mat1)::type>(mat1_desc, MAJOR::COL);
-    // matrix<double> mat2_local = init_local_mat<decltype(mat1)::type>(mat2_desc, MAJOR::COL);
+    prod_block_desc.init(m, n, m, n, IRSRC, ICSRC);
+
+    matrix<double> mat1_local = init_local_mat<decltype(mat1)::type>(mat1_desc, MAJOR::COL);
+    matrix<double> mat2_local = init_local_mat<decltype(mat1)::type>(mat2_desc, MAJOR::COL);
     matrix<double> prod_local = init_local_mat<decltype(mat1)::type>(prod_desc, MAJOR::COL);
 
-    // initialize local matrices with column-major memory arrangement
-    auto mat1_local = get_local_mat(mat1, mat1_desc, MAJOR::COL);
-    auto mat2_local = get_local_mat(mat2, mat2_desc, MAJOR::COL);
-    // auto mat3_local = init_local_mat<decltype(mat1_local)::type>(mat3_desc, MAJOR::COL);
+    // distribute data to other processes
+    linalg::pgemr2d_f(m, k, mat1.c, 1, 1, mat1_block_desc.desc,
+                      mat1_local.c, 1, 1, mat1_desc.desc, blacs_h.ictxt);
+    linalg::pgemr2d_f(k, n, mat2.c, 1, 1, mat2_block_desc.desc,
+                      mat2_local.c, 1, 1, mat2_desc.desc, blacs_h.ictxt);
 
-    printf("mat1_local of %s\n%s", mat1_desc.info().c_str(), str(mat1_local).c_str());
-    printf("mat2_local of %s\n%s", mat2_desc.info().c_str(), str(mat2_local).c_str());
-    // for (int pid = 1; pid != blacs_h.nprocs; pid++)
-    // {
-    //     if (blacs_h.myid == 0)
-    //     {
-    //         // extract matrix from master process
-    //         auto mat1_local = get_local_mat_pid(mat1, mb, kb, IRSRC, ICSRC, blacs_h, pid, MAJOR::COL);
-    //         printf("mat1_local extract from PID 0 for PID %d\n%s", pid, str(mat1_local).c_str());
-    //         // send to the corresponding pid
-    //     }
-    //     else
-    //     {
-    //
-    //     }
-    // }
     // const char transn = 'N';
     // double alpha = 1.0, beta = 0.0;
     // int i1 = 1;
@@ -86,8 +76,13 @@ int main (int argc, char *argv[])
 
     linalg::pgemm_f('N', 'N', m, n, k, 1.0, mat1_local.c, 1, 1, mat1_desc.desc,
                     mat2_local.c, 1, 1, mat2_desc.desc, 0.0, prod_local.c, 1, 1, prod_desc.desc);
+    printf("prod_local on %s\n%s", prod_desc.info().c_str(), str(prod_local).c_str());
+    // gather 
+    linalg::pgemr2d_f(m, n, prod_local.c, 1, 1, prod_desc.desc,
+                      prod.c, 1, 1, prod_block_desc.desc, blacs_h.ictxt);
 
-    printf("prod_local of %s\n%s", prod_desc.info().c_str(), str(prod_local).c_str());
+    if (blacs_h.myid == 0)
+        printf("prod gathered on %s\n%s", prod_desc.info().c_str(), str(prod).c_str());
 
     blacs_h.barrier();
     mpi.finalize();
